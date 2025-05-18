e921eb55a38e31f0cf14874d79f80159edbadf45
aumohite <ashishmohite@gmail.com>
2025-05-18 19:42:25 +0530

    initialcommit

---
+++ src/test/java/com/mohite/springboot_k8s_demo/SpringbootK8sDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohite.springboot_k8s_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootK8sDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
